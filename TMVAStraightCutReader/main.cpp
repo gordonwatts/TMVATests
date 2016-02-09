6385b6692a663629e073ed74c72a720ec7b806f4
Gordon Watts <gordon@gordonandpaula.net>
2016-02-09 22:52:04 +0100

    Added a reader C++ test

---
+++ TMVAStraightCutReader/main.cpp
@@ -0,0 +1,37 @@
+///
+/// Read in a TMVA weight file and do a few tests by hand.
+///
+
+#include "tmva/Reader.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Load up the reader
+	auto reader = new TMVA::Reader();
+
+	// Declare the variables
+	float lowestPtTrack = 0.0;
+	float nTracks = 0.0;
+	float logR = 0.0;
+
+	reader->AddVariable("logR", &logR);
+	reader->AddVariable("nTracks", &nTracks);
+
+	// Book it
+	auto s = "C:\\Users\\gordo\\Documents\\Code\\calratio2015\\JetCutStudies\\SimpleJetCutTraining\\bin\\x86\\Debug\\weights\\VerySimpleTraining_SimpleCuts.weights.xml";
+	reader->BookMVA("SimpleCuts", s);
+
+	// Now get some values
+	logR = 1.0;
+	nTracks = 5;
+	cout << "LogR: " << logR << " nTracks: " << nTracks << ": result=" << reader->EvaluateMVA("SimpleCuts", 0.22) << endl;
+
+	logR = 2.0;
+	nTracks = 00;
+	cout << "LogR: " << logR << " nTracks: " << nTracks << ": result=" << reader->EvaluateMVA("SimpleCuts", 0.22) << endl;
+
+	return 0;
+}
